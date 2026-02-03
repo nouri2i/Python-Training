8f71c80f81e2f69388856e0565efc0d2fbd6ea04
Abdennour Hachemi <abdennour.hachemi@2itesting.com>
2026-02-03 10:43:49 +0000

    update code

--- HeroFactory.py
+++ HeroFactory.py
@@ -13,19 +13,19 @@ It's another day at the lab and you now have all the tools to create the ultimat
 
 - Feel free to add as many abilities to your hero as you like and create multiple heros to practice inheritance
 '''
-
+#Create a class for your superhero and define its stats for strength, speed and whether it can fly or not
 class HeroFactory:
     def __init__(self,name, strength,speed,fly):
         self.name=name
         self.strength=strength
         self.speed=speed
         self.fly=fly
-
+#Create a function perform_ability() that defines what happens when your hero uses their special power
     def perform_ability(self):
         able_to_fly=" and is able to fly."
         ability=f"{self.name} has a  strength of {self.strength} horsepower his speed is {self.speed}"
         if self.fly==True:
             ability+=able_to_fly
         print(ability)
-
+#Create an instance of the class to make the hero come to life
 HeroFactory("Hulk",100, 300, True).perform_ability()
